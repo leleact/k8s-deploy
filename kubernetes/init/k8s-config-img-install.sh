23488c449245bf3e67b8e1992a1f9cdde9e71866
leleact <leleact@gmail.com>
2019-08-13 23:55:31 +0800

    kubernetes: init script mirror update

--- kubernetes/init/k8s-config-img-install.sh
+++ kubernetes/init/k8s-config-img-install.sh
@@ -3,8 +3,11 @@
 images=$(kubeadm config images list| awk -F '/' '{ print $2 }')
 images+=(kubernetes-dashboard-amd64:v1.10.1)
 
+Azure_Mirror=gcr.azk8s.cn/google_containers
+Ali_Mirror=registry.cn-hangzhou.aliyuncs.com/google_containers
+
 for imageName in ${images[@]} ; do
-    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
-    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
-    docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+    docker pull ${Azure_Mirror}/$imageName
+    docker tag  ${Azure_Mirror}/$imageName k8s.gcr.io/$imageName
+    docker rmi  ${Azure_Mirror}/$imageName
 done
