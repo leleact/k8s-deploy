a2ec355fd27228aa3337952af4c1d49021979827
leleact <leleact@gmail.com>
2020-06-13 15:52:52 +0800

    kubernetes: kubernetes image pull bash script modify

--- kubernetes/init/k8s-config-img-install.sh
+++ kubernetes/init/k8s-config-img-install.sh
@@ -2,11 +2,12 @@
 
 images=$(kubeadm config images list| awk -F '/' '{ print $2 }')
 
-Azure_Mirror=gcr.azk8s.cn/google_containers
-Ali_Mirror=registry.cn-hangzhou.aliyuncs.com/google_containers
+#Azure_Mirror=gcr.azk8s.cn/google_containers
+#Ali_Mirror=registry.cn-hangzhou.aliyuncs.com/google_containers
+Mirror=registry.cn-hangzhou.aliyuncs.com/google_containers
 
 for imageName in ${images[@]} ; do
-    docker pull ${Azure_Mirror}/$imageName
-    docker tag  ${Azure_Mirror}/$imageName k8s.gcr.io/$imageName
-    docker rmi  ${Azure_Mirror}/$imageName
+    docker pull ${Mirror}/$imageName
+    docker tag  ${Mirror}/$imageName k8s.gcr.io/$imageName
+    docker rmi  ${Mirror}/$imageName
 done
